8a3be45b82795d2801859ec67a12fe30fc67586f
Samuel Padron <spadron05@gmail.com>
2022-12-08 14:03:48 +0100

    Refactored factor_marginalization to take string input

--- variable_elim.py
+++ variable_elim.py
@@ -44,7 +44,7 @@ class VariableElimination():
         return self.multiply_on_columns(common_columns, factor_a, factor_b)
         
 
-    def factor_marginalization(self, factor:DataFrame, variable:DataFrame) -> DataFrame:
+    def factor_marginalization(self, factor:DataFrame, variable: str) -> DataFrame:
         """compute factor by marginalizing over a variable
 
         Args:
@@ -54,8 +54,7 @@ class VariableElimination():
         Returns:
             DataFrame: _description_
         """
-        variable_name = variable.columns.values[0]
-        columns = [x for x in factor.columns.values if x != 'prob' and x != variable_name]
+        columns = [x for x in factor.columns.values if x != 'prob' and x != variable]
         return factor.groupby(columns, axis=0, as_index=False).sum(numeric_only=True)
         
         
@@ -111,11 +110,11 @@ class VariableElimination():
             factors_including_variable = [factor for factor in factors if next_variable in factor.columns.values]
             
             #get product of factors
-            result = reduce(lambda i, j: self.factor_product(i, j), factors_including_variable)
+            product = reduce(lambda i, j: self.factor_product(i, j), factors_including_variable)
             
+            #sum out the variable
+            result = self.factor_marginalization(product, next_variable)
             print(result)
-            
-            #for factor in factors_with_var:
             
             
         
